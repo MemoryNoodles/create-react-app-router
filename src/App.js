9ddadcd96dfe567703d2bc150994ffad2d17c3e3
noodles <729804002@qq.com>
2022-09-20 21:28:00 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Outlet, Link, useRoutes, useNavigate } from "react-router-dom";
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from '@ant-design/icons';
+import { Layout, Menu } from 'antd';
+import 'antd/dist/antd.css';
+
+
+export const Home = () => {
+  return "home"
 }
+export const About = () => {
+  return "About"
+}
+let routes = [
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/home",
+    element: <Home />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+];
+const menu = [
+  {
+    key: '1',
+    icon: <UserOutlined />,
+    label: 'home',
+    router: "/home"
+  },
+  {
+    key: '2',
+    icon: <VideoCameraOutlined />,
+    label: 'about',
+    router: "/about"
+  },
+
+]
+
+
+const { Header, Sider, Content } = Layout;
+
+const App = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  const navigate = useNavigate()
+  let element = useRoutes(routes);
+
+  return (
+    <Layout style={{ height: "100%" }}>
+      <Sider trigger={null} collapsible collapsed={collapsed} >
+        <div className="logo" />
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={['1']}
+          onClick={(item) => {
+            let route = menu.find(o => o.key == item.key)?.router;
+            navigate(route)
+          }}
+          items={menu}
+        />
+      </Sider>
+      <Layout className="site-layout">
+        <Header className="site-layout-background" style={{ padding: 0 }}>
+          {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+            className: 'trigger',
+            onClick: () => setCollapsed(!collapsed),
+          })}
+        </Header>
+        <Content
+          className="site-layout-background"
+          style={{
+            margin: '24px 16px',
+            padding: 24,
+            minHeight: 280,
+          }}
+        >
+          {element}
+        </Content>
+      </Layout>
+    </Layout>
+  );
+};
+
+
+
 
 export default App;
